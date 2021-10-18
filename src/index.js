19eb7e5b56073d195ce7a49151dbb5f4b007c91d
Lynda <l_bounif@esi.dz>
2021-10-18 18:13:26 +0100

    change server port

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const app = express()
 
-const port = process.env.PORT || 10010
+const port = process.env.PORT || 4000
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 
