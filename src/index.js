b52bc6e0204672445d99469c0d928663008e3b51
Lynda <l_bounif@esi.dz>
2021-10-22 23:34:59 +0100

    change port to test webhook with jenkins

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const app = express()
 
-const port = process.env.PORT || 4001
+const port = process.env.PORT || 4002
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 
